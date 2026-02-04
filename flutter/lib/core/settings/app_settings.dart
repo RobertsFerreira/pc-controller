82dcc2a111071f42df224c225894467c3041815a
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2026-02-03 23:35:17 -0300

    feat(app settings): added settings for url ws

--- flutter/lib/core/settings/app_settings.dart
+++ flutter/lib/core/settings/app_settings.dart
@@ -1,10 +1,12 @@
 import 'package:pc_remote_control/core/settings/retry_config.dart';
 
 class AppSettings {
+  String wsHost = 'localhost';
+  bool wsSecure = false;
   int wsPort = 3000;
   final wsPath = '/ws';
   final RetryConfig retryConfig = RetryConfig();
 
-  String get wsUrl => 'ws://localhost:$wsPort$wsPath';
+  String get wsUrl => '${wsSecure ? 'wss' : 'ws'}://$wsHost:$wsPort$wsPath';
   Duration get wsTimeout => const Duration(seconds: 10);
 }
