8fbda5d14c46b847ef6b8d52d19222394d7714de
Robert Ferreira <robertsilva-ferreira@hotmail.com>
2026-02-03 23:33:42 -0300

    fix(utils): fix possible code cause panic

--- src/modules/core/utils/timestamp_utils.rs
+++ src/modules/core/utils/timestamp_utils.rs
@@ -4,6 +4,6 @@ use std::time::SystemTime;
 pub fn get_timestamp() -> u64 {
     SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
+        .expect("System clock is set before UNIX_EPOCH")
         .as_secs()
 }
